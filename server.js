b9171261881d16d8330c6e2ad8214fb0623cb95a
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-03-07 19:23:31 -1000

    handle initial GET requests

--- server.js
+++ server.js
@@ -6,29 +6,48 @@ const PORT = 8080;
 const server = http
   .createServer((request, response) => {
     const { headers, method, url } = request;
-    let body = [];
-    request
-      .on("error", err => {
-        console.error(err);
-      })
-      .on("data", chunk => {
-        body.push(chunk);
-      })
-      .on("end", () => {
-        body = Buffer.concat(body).toString();
-      });
-
+    request.on("error", err => {
+      console.error(err);
+      response.statusCode = 400;
+      response.end();
+    });
     response.on("error", err => {
       console.error(err);
     });
 
-    response.statusCode = 200;
-    response.setHeader("Content-Type", "application/json");
-
-    const responseBody = { headers, method, url, body };
-
-    response.write(JSON.stringify(responseBody));
-    response.end();
+    switch (request.method === "GET") {
+      case url === "/":
+        fs.readFile("./public/index.html", (err, data) => {
+          if (err) throw err;
+          response.end(data);
+        });
+        console.log("request: ", url);
+        console.log("method: ", request.method);
+        break;
+      case url === "/css/styles.css":
+        fs.readFile("./public/styles.css", (err, data) => {
+          if (err) throw err;
+          response.end(data);
+        });
+        break;
+      case url === "/hydrogen.html":
+        fs.readFile("./public/hydrogen.html", (err, data) => {
+          if (err) throw err;
+          response.end(data);
+        });
+        break;
+      case url === "/helium.html":
+        fs.readFile("./public/helium.html", (err, data) => {
+          if (err) throw err;
+          response.end(data);
+        });
+        break;
+      default:
+        fs.readFile("./public/404.html", (err, data) => {
+          if (err) throw err;
+          response.end(data);
+        });
+    }
   })
   .listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
